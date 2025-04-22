d61ee23a537bd8784e020c101f51bc7e70103fcb
Addow Abdi <adowabdi007@icloud.com>
2025-04-22 06:37:19 -0500

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
+const db = require('../../data/db-config');
+
+function getResources() {
+  return db('resources');
+}
+
+function createResource(resource) {
+  return db('resources')
+    .insert(resource)
+    .then(([id]) => db('resources').where('resource_id', id).first());
+}
+
+module.exports = {
+  getResources,
+  createResource,
+};
 
