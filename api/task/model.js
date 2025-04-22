6d1225ae4b68607bcbffaa3f51d3434e1c0d8f91
Addow Abdi <adowabdi007@icloud.com>
2025-04-22 06:41:05 -0500

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,41 @@
-// build your `Task` model here
+const db = require('../../data/db-config');
+
+function getTasks() {
+  return db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select(
+      't.task_id',
+      't.task_description',
+      't.task_notes',
+      't.task_completed',
+      'p.project_name',
+      'p.project_description'
+    )
+    .then(tasks =>
+      tasks.map(task => ({
+        ...task,
+        task_completed: Boolean(task.task_completed)
+      }))
+    );
+}
+
+function createTask(task) {
+  return db('tasks')
+    .insert(task)
+    .then(([id]) => db('tasks').where('task_id', id).first())
+    .then(async task => {
+      const project = await db('projects').where('project_id', task.project_id).first();
+      return {
+        ...task,
+        task_completed: Boolean(task.task_completed),
+        project_name: project.project_name,
+        project_description: project.project_description,
+      };
+    });
+}
+
+module.exports = {
+  getTasks,
+  createTask,
+};
+
