67038fffdf49e7e5923f1be34a833d284b9793c0
Addow Abdi <adowabdi007@icloud.com>
2025-04-22 06:35:36 -0500

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,25 @@
-// build your `Project` model here
+const db = require('../../data/db-config');
+
+function getProjects() {
+  return db('projects').select('*').then(projects =>
+    projects.map(project => ({
+      ...project,
+      project_completed: Boolean(project.project_completed)
+    }))
+  );
+}
+
+function createProject(project) {
+  return db('projects')
+    .insert(project)
+    .then(([id]) => db('projects').where('project_id', id).first())
+    .then(project => ({
+      ...project,
+      project_completed: Boolean(project.project_completed)
+    }));
+}
+
+module.exports = {
+  getProjects,
+  createProject,
+};
